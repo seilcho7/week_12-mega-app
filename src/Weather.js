185825420941a3e89e184bb5e0cf45b6d1a48e8a
Seil Cho <seilcho7@hotmail.com>
2019-04-29 15:23:38 -0400

    adds weather app

---
+++ src/Weather.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import axios from 'axios';
+
+class Weather extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            zip: '',
+            weather: [],
+            text: ''
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h2>Weather</h2>
+                <input type="number" onChange={(e) => {this._setZip(e.target.value)}} placeholder="Weather by zip code"></input>
+                <ul>
+                    {
+                        this.state.weather.main ? [<li key={'city'}>City: {this.state.weather.name}</li>, <li key={'temp'}>Temp: {this.state.weather.main.temp}</li>, <li key={'weather'}>Weather: {this.state.weather.weather[0].main}</li>] : null
+                    }
+                </ul>
+            </div>
+        )
+
+    }
+
+    async componentDidMount() {
+        if (this.state.zip && this.state.zip.length === 5) {
+            try {
+                const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${this.state.zip}&APPID=544e633f1a1d6d7bb3378ec526f12f59`);
+                this.setState({
+                    weather: response.data
+                })
+            } catch(error) {
+                return error
+            }
+        }
+    }
+
+    _setZip = (zip) => {
+        this.setState({
+            zip
+        }, this.componentDidMount)
+    } 
+
+    // _getWeather = async () => {
+    //     const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=544e633f1a1d6d7bb3378ec526f12f59`);
+    //     console.log(response.list);
+    //     this.setState({
+    //         weather: response
+    //     })
+    // }
+}
+
+export default Weather;
